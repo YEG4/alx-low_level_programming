2f531065f7e51190be873ce8875280674c75bc15
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-10 13:11:58 -0700

    Created 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *main - prints a string on the console
+ * Return 0 if succesful
+ */
 
-int main(void){
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
