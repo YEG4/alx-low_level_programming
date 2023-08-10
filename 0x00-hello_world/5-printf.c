2e1fb4dca9712f244ba2ae9675240a29a4139556
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-10 13:24:28 -0700

    Created 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   * Description: 'prints string on the console.'
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * Description: 'prints string on the console.'
+ * Return: Always 0 (Success)
+ *     
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
