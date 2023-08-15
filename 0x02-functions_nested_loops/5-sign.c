5791afdf9dec4cbd4a958f1001127d46d26e3e22
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-15 09:05:45 -0700

    Created 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @n: letter to be checked
+ *
+ * Description: 'Prints sign of a number.'
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		value = 1;
+	}
+	else if (n  < 0)
+	{
+		_putchar('-');
+		value = -1;
+	}
+	else
+	{
+		_putchar('0');
+		value = 0;
+	}
+	return (value);
+}
