8b27f6ac5b6034a8da742273d696f265660f277c
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-15 12:55:17 -0700

    Created 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function
+ * @x, y: Two numbers to be added.
+ *
+ * Description: 'Adds two numbers and returns the result
+ * Return: Always result (Success)
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
