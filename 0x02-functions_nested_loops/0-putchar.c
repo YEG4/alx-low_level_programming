ebfb9db08a1cf1b03c4d17245f20e20c52508af9
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-15 08:24:11 -0700

    Modified 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,18 @@
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+#include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char arr[] = "_putchar";
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
