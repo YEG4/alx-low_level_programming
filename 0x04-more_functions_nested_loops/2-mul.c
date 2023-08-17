059c4f6fb396e376688d4fe8d2218f9055315b2d
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-17 08:21:27 -0700

    Created 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - Multiplies a and b.
+ * @a: first number
+ * @b: second number
+ *
+ * Description: 'prints string on the console.'
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return a * b;
+}
