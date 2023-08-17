d6a4e1f6403003b4b11b2de3108f8f66389c5fe7
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-17 11:03:27 -0700

    Created 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,22 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, counter, counter_size = size;
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		
+		for (i =1; i <= size; i++)
 		{
-			for (j = 0; j < size - 1; j++)
+			counter = i;
+			for (j = 0; j < counter_size - 1; j++)
 				_putchar(' ');
-			_putchar('#');
+			counter_size--;
+			while (counter != 0)
+			{
+				_putchar('#');
+				counter--;
+			}
 			_putchar('\n');
 		}
 	}
