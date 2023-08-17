21a9c36658951c0e20fa7e0636665045a881df92
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-17 08:53:41 -0700

    Created 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers- prints numbers from 0 to 14.
+ *
+ * Description: 'prints numbers from 0 to 14.'
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i > 9)
+		{
+			_putchar(49);
+			i %= 10;
+		}
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
