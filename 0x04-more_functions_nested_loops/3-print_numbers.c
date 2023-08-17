5625aec66b016dc0e309488cca1b684ed7f9707e
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-17 08:24:56 -0700

    Created 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+/**
+ * print_numbers - prints numbers from 0 to 9.
+ *
+ * Description: 'prints numbers from 0 to 9.'
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + 48);
+	_putchar('\n');
+}
