ed76eeb8688be88c0bb49513431c1704ae13f35a
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-17 09:17:43 -0700

    Created 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+/**
+ * print_line- prints a line _____.
+ * @n: number of '_' to be printed
+ * Description: 'Prints a line on the console'
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
