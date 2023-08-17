828a8a93e79dccd13b5d3d222a23d7594e078290
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-17 08:31:55 -0700

    Created 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * print_most_numbers- prints most of the numbers
+ *
+ * Description: 'prints string on the console.'
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ( i != 2 || i != 4)
+			_putchar(i + 48);
+
+	}
+	_putchar('\n');
+}
