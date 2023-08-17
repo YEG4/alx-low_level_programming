61258d45505ba6ec380af2163b6e0144de13e78f
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-17 11:29:55 -0700

    Created 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,53 +9,5 @@
 
 void print_number(int n)
 {
-	if (n >= 0)
-	{
-		if (n > 9 && n < 100)
-		{
-			_putchar(n / 10 + 48);
-			_putchar(n % 10 + 48);
-		}
-		else if (n >= 100 && n < 1000)
-		{
-			_putchar((n / 100) + 48);
-			_putchar((n / 10) % 10 + 48);
-			_putchar(n % 10 + 48);
-		}
-		else if (n >= 1000 && n < 10000)
-		{
-			_putchar((n / 1000) + 48);
-			_putchar((n / 100) % 10 + 48);
-			_putchar((n / 10) % 10 + 48);
-			_putchar(n % 10 + 48);
-		}
-		else if (n < 9)
-			_putchar(i + 48);
-	}
-	else
-	{
-			if (n < -9 && n > -100)
-		{
-			_putchar('-');
-			_putchar(n / 10 + 48);
-			_putchar(n % 10 + 48);
-		}
-		else if (n <= -100 && n > -1000)
-		{
-			_putchar('-');
-			_putchar((n / 100) + 48);
-			_putchar((n / 10) % 10 + 48);
-			_putchar(n % 10 + 48);
-		}
-		else if (n <= -1000 && n > -10000)
-		{
-			_putchar('-');
-			_putchar((n / 1000) + 48);
-			_putchar((n / 100) % 10 + 48);
-			_putchar((n / 10) % 10 + 48);
-			_putchar(n % 10 + 48);
-		}
-		else if (n > -9)
-			_putchar(i + 48);
-	}
+	
 }
