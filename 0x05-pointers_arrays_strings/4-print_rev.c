5053514939d4f9859321c8a47e24f082badd5369
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-21 10:49:55 -0700

    Created 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+/**
+ * print_rev - Prints string in reverse.
+ * @s: string to be reversed
+ *
+ * Description: 'Prints string in reverse on the console.'
+ */
+
+
+void print_rev(char *s)
+{
+	int counter = 0;
+
+	while (*s != 0)
+	{
+		counter++;
+		s++;
+	}
+	while (counter != 0)
+	{
+		_putchar(*s);
+		counter--;
+		s--;
+	}
+	_putchar('\n');
+}
