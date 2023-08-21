0518e9427ecf29c7bf9328d086602a10a4fa2d8a
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-21 09:42:06 -0700

    Created 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - Changes the value of the number.
+ * @n: number to be updated to 98.
+ *
+ * Description: 'Updates the value of n to 98.'
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
