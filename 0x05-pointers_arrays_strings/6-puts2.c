f3174d0aaa0fa83b0dbf904ebd296e589e0c7428
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-21 11:27:44 -0700

    Created 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2- Prints Every other character.
+ * @str: string to be printed.
+ *
+ * Description: 'Prints even index of a string.'
+ */
+
+
+void puts2(char *str)
+{
+	int counter = 0;
+	while (*str != 0)
+	{
+		if (counter % 2 == 0)
+			_putchar(*str);
+		counter++;
+		str++;
+	}
+	_putchar('\n');
+}
