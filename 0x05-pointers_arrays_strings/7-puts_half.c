b1e60d1c1f4ce4048e675b5ab4e488af0915750d
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-21 11:46:20 -0700

    Created 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of the string.
+ * @str: string to be printed.
+ *
+ * Description: 'Prints the second half of the string.'
+ */
+
+
+void puts_half(char *str)
+{
+	int counter = 0, mid;
+	char *ptr = s;
+
+	while (*ptr != 0)
+	{
+		counter++;
+		ptr++;
+	}
+	mid = counter / 2;
+	while (mid != 0)
+	{
+		str++;
+		mid--;
+	}
+	while (*str != 0)
+	{
+		_putchar(*s);
+		str++;
+	}
+	_putchar('\n');
+}
