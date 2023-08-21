eae1a676a04c91b2895a21a22af6ae38da7a6cd7
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-21 11:59:50 -0700

    Created 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+/**
+ * print_array- Prints elements in the array.
+ * @a: array to be printed
+ * @n: number of elements in the array
+ *
+ * Description: 'Prints elements in the array'
+ */
+
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%i, ", a[i]);
+	}
+	printf("\n");
+}
