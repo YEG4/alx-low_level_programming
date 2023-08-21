802130891f35a9390d8808e91968c835bc8d0f50
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-21 12:07:20 -0700

    Created 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strcpy- Copies one string and store it in another.
+ * @dest: destination string.
+ * @stc: String to be copied to dest.
+ *
+ * Description: 'Copies one string and store it in another.'
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != 0)
+	{
+		*dest = *src;
+		src++;
+		dest++
+	}
+	*dest = '\0';
+}
