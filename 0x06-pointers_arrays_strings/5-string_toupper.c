f444ee19719e2fb840a4ca6295d5bd44fea28765
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-24 02:13:47 -0700

    Created 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+/**
+ * string_toupper - Changes all lowercase to upper.
+ * @s: string to be modified.
+ *
+ * Description: 'Changes all lowercase letters to uppercase.'
+ * Return: Address of string.
+ */
+
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] = s[i];
+		else if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		else
+	}
+	return (s);
+}
