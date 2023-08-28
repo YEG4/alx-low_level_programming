95a4c5eaf766e426d1bc8bb29580eb0223518a68
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-28 14:02:02 -0700

    Created 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _memcpy - Entry point
+ * @dest: Address of destination.
+ * @src: Address of source
+ * @n: number of of bytes.
+ *
+ * Description: 'Copies from source to destination.'
+ * Return: Address pointed by destination .
+ */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		n--;
+		dest[i] = src[i];
+	}
+	return (s);
+}
