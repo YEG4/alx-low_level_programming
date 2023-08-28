7d20131cd33af4fb7afbfa985d7a5299f219fd49
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-08-28 13:46:08 -0700

    First commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ * @s: Address
+ * @b: constant byte.
+ * @n: number of of bytes.
+ *
+ * Description: 'Write a constant byte strating from address pointed by s.'
+ * Return: Address pointed by s .
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+
+		s[i] = b;
+	return (s);
+}
