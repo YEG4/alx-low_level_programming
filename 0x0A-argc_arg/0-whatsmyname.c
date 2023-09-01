528af39ba59c3bd1f44b19dc899745a2daff2bb9
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-01 15:37:31 -0700

    First commit

---
+++ 0x0A-argc_arg/0-whatsmyname.c
@@ -0,0 +1,14 @@
+/**
+ * main - Entry point
+ * @argc: arguement count.
+ * @argv: arguement vector.
+ *
+ * Description: 'Prints name of file on console.'
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
