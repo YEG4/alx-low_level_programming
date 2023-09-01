f21ebcdbcf9a84b4c2cd85063f636536c528771b
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-01 16:25:31 -0700

    Created 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arguement count.
+ * @argv: arguement vector.
+ *
+ * Description: 'Prints name of file on console.'
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0 && argc <= 2)
+	{
+		printf("%d\n", argv[1] * argv[2]);
+	}
+	return (argv[1] * argv[2]);
+}
