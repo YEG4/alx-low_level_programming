49f8b4eba13f4f6edfd8f7eb05be0695719197fb
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-01 16:04:51 -0700

    Created 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arguement count.
+ * @argv: arguement vector.
+ *
+ * Description: 'Prints name of file on console.'
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argc - 1);
+	return (0);
+}
