c8dd3f9703f7e5ffec1d8dd45621c41fe02b29b1
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-01 16:32:59 -0700

    Created 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -15,7 +16,7 @@ int main(int argc, char *argv[])
 
 	if (argc > 0 && argc <= 2)
 	{
-		printf("%d\n", (argv[1] + 48) * (argv[2] + 48));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		sum = (argv[1] * argv[2]);
 	}
 	else
