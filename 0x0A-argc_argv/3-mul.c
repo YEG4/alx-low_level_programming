0c3e761c31cbe5f51619e61ca4192b21ce7b6114
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-01 16:30:28 -0700

    Created 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,17 @@
 
 int main(int argc, char *argv[])
 {
+	int sum;
+
 	if (argc > 0 && argc <= 2)
 	{
-		printf("%d\n", argv[1] * argv[2]);
-		return (argv[1] * argv[2]);
+		printf("%d\n", (argv[1] + 48) * (argv[2] + 48));
+		sum = (argv[1] * argv[2]);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
