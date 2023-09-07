97136e7262701cb042c7674ff7e21525bc4c62f5
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-07 11:21:21 -0700

    dsada

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 void malloc_checked(unsigned int b);
 charstring_nconcat(char s1, chars2, unsigned int n);
 void _calloc(unsigned int nmemb, unsigned int size);
