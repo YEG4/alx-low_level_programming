7b80f13dbf86fbdfdec5d3af1c0c3a2782c834f2
ATeight <[200~ATeight>
2023-09-07 23:36:13 +0300

    Modified 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,12 +11,14 @@
 
 int *array_range(int min, int max)
 {
-	int *ptr; 
+	int *ptr, i, j;
 
 	if (max - min < 0)
 		return (NULL);
 	ptr = malloc((max - min) * sizeof(*ptr));
 	if (ptr == NULL)
 		return (NULL);
+	for (i = min, j = 0; i <= max; i++, j++)
+		ptr[j] = i;
 	return (ptr);
 }
