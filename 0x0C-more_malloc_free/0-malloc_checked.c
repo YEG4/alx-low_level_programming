9816687a0da3da6324afec7c270c25ca4a4ef4a2
ATeight <[200~ATeight>
2023-09-07 20:49:49 +0300

    Modified 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,3 +5,11 @@
  * Return: a pointer to the allocated memory
  */
 
+void *malloc_checked(unsigned int b)
+{
+    void *ptr = malloc(b);
+    
+    if (ptr == NULL)
+        return (void *)98;
+    return (ptr);
+}
