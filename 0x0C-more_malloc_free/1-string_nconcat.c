db20c5e0b5f2638c2aa7f27b381369c623a8ca8d
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-07 11:59:25 -0700

    Created 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,38 +9,30 @@
  *
  * Return: pointer to the resulting string
  */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)       
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_string;
-	unsigned int counter1 = 0, counter2 = 0, size1 = 0, size2 = 0;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[size1])
-		size1++;
-	while (s2 && s2[size2])
-		size2++;
-
-	if (n < size2)
-		new_string = malloc(sizeof(char) * (size1 + n + 1));
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		new_string = malloc(sizeof(char) * (size1 + size2 + 1));
-
-	if (!new_string)
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
 		return (NULL);
-
-	while (counter1 < size1)
+	while (i < len1)
 	{
-		new_string[counter1] = s1[counter1];
-		counter1++;
+		s[i] = s1[i];
+		i++;
 	}
-
-	while (n < size2 && counter1 < (size1 + n))
-		new_string[counter1++] = s2[counter2++];
-
-	while (n >= size2 && counter1 < (size1 + size2))
-		new_string[counter1++] = s2[counter2++];
-
-	new_string[counter1] = '\0';
-
-	return (new_string);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
