7ebff8ccb397145e8a2fcdfef21714f5d4d66665
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-07 10:12:55 -0700

    First Commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,8 @@
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void malloc_checked(unsigned int b);
+charstring_nconcat(char s1, chars2, unsigned int n);
+void _calloc(unsigned int nmemb, unsigned int size);
+intarray_range(int min, int max);
+#endif
