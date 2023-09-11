d965d747e064c18b8117e6211fa9d172add7915a
ATeight <[200~ATeight>
2023-09-11 22:30:45 +0300

    Created 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * print_dog - Prints a struct
+ * @d: pointer to struct
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+// do nothing
+printf("Name: %s\n", d->name);
+printf("Age: %0.1f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
