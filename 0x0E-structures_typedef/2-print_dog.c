ec41f95fee838a75d4c3505ae560412ad18ac36c
ATeight <[200~ATeight>
2023-09-11 23:08:42 +0300

    Modified 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,10 @@ if (d->name == NULL)
 {printf("Name: (nil)\n"); }
 else
 {printf("Name: %s\n", d->name); }
-printf("Age: %f\n", d->age);
+if (d->age == 0)
+{printf("Age: (nil)\n");}
+else
+{printf("Age: %f\n", d->age);}
 if (d->owner == NULL)
 {printf("Owner: (nil)\n"); }
 else
