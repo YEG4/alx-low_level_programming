4b5a6b0cf6582318abfe05262967b7bd0d86ef16
ATeight <[200~ATeight>
2023-09-11 22:23:31 +0300

    Created 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - defines a struct
  * @d: pointer to struct
@@ -9,6 +10,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d == NULL)
+        d = malloc(sizeof(struct dog));
 d->name = name;
 d->age = age;
 d->owner = owner;
