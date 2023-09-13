e7bc845e63fafcd3afbb2d3d1c8043c21371be1a
ATeight <[200~ATeight>
2023-09-13 23:24:35 +0300

    Created 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - Call different function on an array.
+ * @array: array of int.
+ * @size: size of the array.
+ * @action: pointer to call different functions.
+ *
+ * Description: 'Call different function on an array.'
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+if (array == 0 || action == 0)
+{return;}
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
