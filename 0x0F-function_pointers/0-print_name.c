e87af017b2cc5fc862b91121ee5336d598c665a7
ATeight <[200~ATeight>
2023-09-14 01:17:15 +0300

    Modified

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,4 @@
 #include "function_pointers.h"
-
-
 /**
  * print_name - Prints the first argument to console.
  * @name: pointer to char.
@@ -14,5 +12,4 @@ void print_name(char *name, void (*f)(char *))
 if (name == 0 || f == 0)
 {return; }
 f(name);
-return;
 }
