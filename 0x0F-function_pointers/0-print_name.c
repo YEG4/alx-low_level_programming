020aa29d5e15b29d18e3033818b89d1104eb430c
ATeight <[200~ATeight>
2023-09-13 22:52:44 +0300

    First Commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - Prints the first argument to console.
+ * @name: pointer to char.
+ * @f: pointer to function that takes a char and returns nothing.
+ *
+ * Description: 'prints string on the console.'
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
