b3b7cffa2ddf398ce1f6a29697ed6f9504367ff5
ATeight <[200~ATeight>
2023-09-14 01:09:09 +0300

    Modified 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+
 
 /**
  * print_name - Prints the first argument to console.
@@ -11,7 +11,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-if (!name || !f)
+if (name == 0 || f == 0)
 {return; }
 f(name);
 }
+ 
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef __FPOINTERS__
 #define __FPOINTERS__
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
