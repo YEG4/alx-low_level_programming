707612a2ec206734df1e2d5af16587124c10f8ed
ATeight <[200~ATeight>
2023-09-14 01:26:41 +0300

    Modified

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef __FPOINTERS__
 #define __FPOINTERS__
-#include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
