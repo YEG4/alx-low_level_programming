cfb5402bbd645126f7e4e77138b8b443634eb007
ATeight <[200~ATeight>
2023-09-15 00:50:03 +0300

    Created 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+/**
+ * print_all - print all arguments passed to it.
+ * @format: pointer to char.
+ *
+ * Description: 'Adds all arguments .'
+ */
+
+void print_all(const char * const format, ...)
+{
+char *string, *separator = "";
+unsigned int i;
+va_list args;
+va_start(args,format);
+
+i = 0;
+if (format)
+{
+while (format[i] != 0)
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", separator, va_arg(args,int));
+break;
+case 'i':
+printf("%s%d", separator, va_arg(args,int));
+break;
+case 'f':
+printf("%s%f", separator, va_arg(args,float));
+break;
+case 's':
+string = va_arg(args, char *);
+if (string == 0)
+string = "(nil)";     
+printf("%s%s", separator, string);
+break;
+default:
+i++;
+break;
+}
+i++;
+separator = ", ";
+}
+}
+va_end(args);
+printf("\n");
+}
