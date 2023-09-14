b6327e3519e42e0f26ecf89289e41193a8c1040c
ATeight <[200~ATeight>
2023-09-14 23:50:28 +0300

    Created 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,44 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print numbers with separator passed to it.
+ * @separator: pointer to separator.
+ * @n: number of arguments will be passed.
+ *
+ * Description: 'Adds all arguments .'
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+int number, counter, i;
+va_start(args, n);
+counter = n;
+i = 0;
+if (separator == NULL)
+{
+while(counter != 0)
+{
+number = va_arg(args, int);
+printf("%d", number);
+counter--;
+}
+}
+else
+{
+while(counter != 0)
+{
+number = va_arg(args, int);
+printf("%d", number);
+if (counter != 1)
+{
+while (separator[i] != 0)
+{
+printf("%c", separator[i]);
+i++;
+}
+}
+counter--;
+}
+}
+printf("\n");
+}
