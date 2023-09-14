c0f863c017c5b9238e9f2a9fe2a09d4e870a6c07
ATeight <[200~ATeight>
2023-09-15 00:30:56 +0300

    Created 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print strings with separator passed to it.
+ * @separator: pointer to separator.
+ * @n: number of arguments will be passed.
+ *
+ * Description: 'Adds all arguments .'
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+char *string;
+unsigned int i;
+
+
+va_list args;
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+{
+string = va_arg(args, char *);
+if (!string)
+string = "(nil)";
+if (!separator)
+printf("%s", string);
+else if (separator && i == 0)
+printf("%s", string);
+else
+printf("%s%s", separator, string);
+}
+
+printf("\n");
+
+va_end(args);
+}
