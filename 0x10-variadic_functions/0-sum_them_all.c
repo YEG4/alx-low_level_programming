6b53dc9c8c08fa47ccfdd80b996cd29db69992b9
ATeight <[200~ATeight>
2023-09-14 23:04:59 +0300

    First Commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Adds all arguments 
+ * @n: number of arguments.
+ * 
+ *
+ * Description: 'Adds all arguments .'
+ * Return: Sum.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+
+va_start(args, n);
+if (n == 0)
+{return (0); }
+int n1 = va_arg(args, int);
+int n2 = va_arg(args, int);
+return (n1 + n2);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef __VARFUNC__
+#define __VARFUNC__
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif
