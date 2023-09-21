a53e2838914e434620af4b5aeb0bb2b78e03eeaa
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-21 22:31:30 +0300

    Created 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node to the beginning of the list.
+ * @str: pointer to a Node
+ * @head: pointer to pointer to Node
+ * 
+ * Description: adds a new node to the beginning of the list.
+ * Return: the address of the new element , or (NULL) if it failed.
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *temp = *head;
+
+	if (new_node == NULL)
+		return (NULL);
+	*head = new_node;
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = temp;
+
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef __LINKED_LIST__
 #define __LINKED_LIST__
 #include <stdio.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,5 +20,6 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* __LINKED_LIST__ */
