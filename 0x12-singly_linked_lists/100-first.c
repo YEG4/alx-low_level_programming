d04c73c24ac6b70f21dc1bc5b6883fd85d94a14a
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-21 23:46:34 +0300

    Created 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * constructor - exectues before main function.
+ *
+ * Description: frees a list_t list.
+ */
+
+void _constructor_(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
