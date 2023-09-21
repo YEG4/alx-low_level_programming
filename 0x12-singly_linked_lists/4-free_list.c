2501dc35042ab81076f116344c8984d1942d9ef2
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-21 23:12:22 +0300

    Created 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: pointer to pointer to Node
+ *
+ * Description: frees a list_t list.
+ */
+
+
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		free(head);
+	}	
+}
