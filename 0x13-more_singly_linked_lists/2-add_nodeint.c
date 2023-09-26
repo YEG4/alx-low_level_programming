4945d80d436d7382a3944f56134551c75e8332f0
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-26 22:12:02 +0300

    Created2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_nodeint - print member of linked list.
+ * @h: pointer to listint_t.
+ * @n: elemnt of the node.
+ * 
+ * Description: print member of linked list.
+ * Return: address of new node.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
