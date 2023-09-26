bedf85745b2a207e70ca2e1f5ed3d9129a8a4a18
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-26 22:54:52 +0300

    Created 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint - print member of linked list.
+ * @head: pointer to listint_t.
+ *
+ *
+ * Description: print member of linked list.
+ * Return: address of new node.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head->next != NULL)
+	{
+		head->next = temp;
+		free(head);
+		head = temp;
+	}
+	free(head);
+}
