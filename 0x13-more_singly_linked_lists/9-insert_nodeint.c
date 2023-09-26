54f63e843ba4f36094d79762549280186c724a93
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-26 23:29:25 +0300

    UPLOADED

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *next_node = *head;
 
 	new_node = malloc(sizeof(listint_t));
-	if (!head ||!new_node)
+	if (!head || !new_node)
 		return (NULL);
 
 	new_node->n = n;
