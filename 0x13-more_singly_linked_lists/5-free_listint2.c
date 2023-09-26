cbec8457d98c40dea7fd0c7c222e33af160654f3
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-26 23:03:47 +0300

    Created 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * free_listint2 - print member of linked list.
+ * @head: pointer to listint_t.
+ *
+ *
+ * Description: print member of linked list.
+ * Return: address of new node.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return;
+	while ((*head)->next != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	free(head);
+	*head = NULL;
+}
