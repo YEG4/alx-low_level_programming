91551e32369d19d82c76c9f2691bb4299c0c8f12
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-26 23:09:07 +0300

    Created 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,7 +12,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 	while ((*head)->next != NULL)
 	{
@@ -20,6 +20,6 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp;
 	}
-
+	free(*head);
 	*head = NULL;
 }
