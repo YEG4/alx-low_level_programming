8c9a9eaee54ee6a80ef42554477e63ff2c129cef
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-29 19:46:54 +0300

    Created 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - Check if it's Big or litte endian
+ * 
+ * Description: Check if it's Big or litte endian
+ * Return: 1 or 0.
+ */
+int get_endianness(void)
+{
+	int number;
+
+	number = 8;
+	if (*(int *)&number == 8)
+		return (1);
+	else
+		return (0);
+}
