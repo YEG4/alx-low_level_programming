9cd69130d19c530b2ae4279f77113c15d061c1aa
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-29 19:18:25 +0300

    First Commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * binary_to_uint - convert a binary number to int
+ * Return: number in decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum, p;
+	int width;
+
+	if (b == NULL)
+		return (0);
+
+	for (width = 0; b[width]; width++)
+	{
+		if (b[width] != '0' && b[width] != '1')
+			return (0);
+	}
+
+	for (p = 1, sum = 0, width--; width >= 0; width--, p *= 2)
+	{
+		if (b[width] == '1')
+			sum += p;
+	}
+
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* __MAIN_H__ */
