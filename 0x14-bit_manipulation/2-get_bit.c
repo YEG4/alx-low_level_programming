b6103ad20ead75219f269387fcea9f831ccfeeb2
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-09-29 19:31:27 +0300

    Created 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - return bit at a given index.
+ * @n: decimal number.
+ * @index: place to retrieve bit.
+ *
+ * Description: return bit at a given index.
+ * Return: number at the index or -1.
+ */
+
+
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 31)
+		return (-1);
+	
+	return ((n & (~(1 << index))) >> index );
+}
