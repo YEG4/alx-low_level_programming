34672b061cec990aecaecb8ae4b3e7c364ead4eb
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-10-07 13:23:33 +0300

    Updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,19 +9,19 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int op, wr, len = 0;
+	int op, wr, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
 	op = open(filename, O_WRONLY | O_APPEND);
-	wr = write(op, text_content, len);
+	wr = write(op, text_content, length);
 
 	if (op == -1 || wr == -1)
 		return (-1);
